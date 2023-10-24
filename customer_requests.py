d0c67244de5140adc7d9dd9609207e71de7e2161
Olivia Daniels <128088750+danielsolivia4199@users.noreply.github.com>
2023-10-24 16:06:50 -0500

    Add files via upload

--- customer_requests.py
+++ customer_requests.py
@@ -1,3 +1,6 @@
+import json
+import sqlite3
+from models import Customer
 CUSTOMERS = [
     {
         "id": 1,
@@ -13,17 +16,81 @@ CUSTOMERS = [
     }
 ]
 
-def get_all_customers():
-    return CUSTOMERS
+# def get_all_customers():
+#     return CUSTOMERS
 
+def get_all_customers():
+    # Open a connection to the database
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.address,
+            a.email,
+            a.password
+        FROM customer a
+        """)
+
+        # Initialize an empty list to hold all animal representations
+        customers = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Animal class above.
+            customer = Customer(row['id'], row['name'], row['address'], row['email'], row['password'])
+
+            customers.append(customer.__dict__) # see the notes below for an explanation on this line of code.
+
+    return customers
+
+# def get_single_customer(id):
+#     requested_customer = None
+    
+#     for customer in CUSTOMERS:
+#         if customer["id"] == id:
+#             requested_customer = customer
+    
+#     return requested_customer
 def get_single_customer(id):
-    requested_customer = None
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement.
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.address,
+            a.email,
+            a.password
+        FROM customer a
+        WHERE a.id = ?
+        """, ( id, ))
+
+        # Load the single result into memory
+        data = db_cursor.fetchone()
+
+        # Create an animal instance from the current row
+        customer = Customer(data['id'], data['name'], data['address'], data['email'], data['password'])
+
+        return customer.__dict__
     
-    for customer in CUSTOMERS:
-        if customer["id"] == id:
-            requested_customer = customer
-    
-    return requested_customer
 
 def create_customer(customer):
     # Get the id value of the last animal in the list
--- locations_requests.py
+++ locations_requests.py
@@ -1,3 +1,6 @@
+import sqlite3
+import json
+from models import Location
 LOCATIONS = [
     {
         "id": 1,
@@ -11,21 +14,81 @@ LOCATIONS = [
     }
 ]
 
-def get_all_locations():
-    return LOCATIONS
+# def get_all_locations():
+#     return LOCATIONS
 
-#Function with a single parameter
-def get_single_location(id):
-  #Variable to hold the found locations, if it exists
-  requested_location = None
+# #Function with a single parameter
+# def get_single_location(id):
+#   #Variable to hold the found locations, if it exists
+#   requested_location = None
   
-  # Iterate the LOCATIONS list above. Very similar to the for ...of loops in JS
-  for location in LOCATIONS:
-    #dictionaries in python use [] notation to find a key instead of the dot notation in JS
-    if location["id"] == id:
-        requested_location = location
+#   # Iterate the LOCATIONS list above. Very similar to the for ...of loops in JS
+#   for location in LOCATIONS:
+#     #dictionaries in python use [] notation to find a key instead of the dot notation in JS
+#     if location["id"] == id:
+#         requested_location = location
         
-  return requested_location
+#   return requested_location
+
+def get_all_locations():
+    # Open a connection to the database
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.address
+        FROM location a
+        """)
+
+        # Initialize an empty list to hold all animal representations
+        locations = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Animal class above.
+            location = Location(row['id'], row['name'], row['address'])
+
+            locations.append(location.__dict__) # see the notes below for an explanation on this line of code.
+
+    return locations
+
+def get_single_location(id):
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement.
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.address
+        FROM location a
+        WHERE a.id = ?
+        """, ( id, ))
+
+        # Load the single result into memory
+        data = db_cursor.fetchone()
+
+        # Create an animal instance from the current row
+        location = Location(data['id'], data['name'], data['address'])
+
+        return location.__dict__
 
 def create_location(location):
     # Get the id value of the last animal in the list
